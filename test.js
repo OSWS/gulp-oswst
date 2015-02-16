93729b88bbfaee5290c9ca45d9b8fa55e0052d76
ivansglazunov <ivansglazunov@gmail.com>
2015-02-16 07:24:28 +0000

    Add types tests.

--- test.js
+++ test.js
@@ -4,16 +4,34 @@ var through = require('through2');
 var assert = require('chai').assert;
 
 describe('gulp-osws-templates', function() {
-    it('compile', function(done) {
-        gulp.src(__dirname + '/tests/module.js')
+    it('mixin', function(done) {
+        gulp.src(__dirname + '/tests/mixin.js')
         .pipe(templates({
             arguments: [2],
             context: { b: 3 }
         }))
         .pipe(through.obj(function(file, enc, callback){
-            assert.equal(file.path, __dirname + '/tests/module.html');
+            assert.equal(file.path, __dirname + '/tests/mixin.html');
             assert.equal(String(file.contents), '123');
             done();
         }));
-    })
+    });
+    it('tag', function(done) {
+        gulp.src(__dirname + '/tests/tag.js')
+        .pipe(templates({ context: { a: 2 }}))
+        .pipe(through.obj(function(file, enc, callback){
+            assert.equal(file.path, __dirname + '/tests/tag.html');
+            assert.equal(String(file.contents), '<div>123</div>');
+            done();
+        }));
+    });
+    it('data', function(done) {
+        gulp.src(__dirname + '/tests/data.js')
+        .pipe(templates({ context: { a: 3 }}))
+        .pipe(through.obj(function(file, enc, callback){
+            assert.equal(file.path, __dirname + '/tests/data.html');
+            assert.equal(String(file.contents), '123');
+            done();
+        }));
+    });
 });
---
+++ tests/data.js
@@ -0,0 +1,7 @@
+var T = require('osws-templates');
+
+with (T.with) {
+
+module.exports = data(12, '<%= a %>');
+
+}
